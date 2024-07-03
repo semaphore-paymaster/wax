a1530493c7cc235e0b958846b7b0ef5380de5de0
zkFriendly <zkfriendly@proton.me>
2024-07-03 11:41:41 +0200

    feat: gatekeeper validation and initialization

--- packages/plugins/src/paymaster/Gatekeeper.sol
+++ packages/plugins/src/paymaster/Gatekeeper.sol
@@ -1,15 +1,15 @@
 pragma solidity 0.8.23;
 
-import "./interfaces/ISemaphore.sol";
-import "./interfaces/IPoap.sol";
+import './interfaces/ISemaphore.sol';
+import './interfaces/IPoap.sol';
 
 contract PoapSemaphoreGatekeeper {
     address private immutable _semaphore;
     address private immutable _poap;
     uint256 private immutable _eventId;
 
-    uint256 private  _groupId;
-    
+    uint256 private _groupId;
+
     error InvalidToken();
 
     constructor(address __semaphore, address __poap, uint256 __eventId) {
@@ -22,9 +22,7 @@ contract PoapSemaphoreGatekeeper {
         _groupId = ISemaphore(_semaphore).createGroup(address(this));
     }
 
-    function validate(
-        ISemaphore.SemaphoreProof calldata proof
-    ) external returns (bool) {
+    function validate(ISemaphore.SemaphoreProof calldata proof) external returns (bool) {
         if (ISemaphore(_semaphore).verifyProof(_groupId, proof)) {
             ISemaphore(_semaphore).validateProof(_groupId, proof);
             return true;
@@ -33,10 +31,7 @@ contract PoapSemaphoreGatekeeper {
     }
 
     function enter(uint256 _tokenIndex, uint256 _identityCommitment) external {
-        (, uint256 eventId) = IPoap(_poap).tokenDetailsOfOwnerByIndex(
-            msg.sender,
-            _tokenIndex
-        );
+        (, uint256 eventId) = IPoap(_poap).tokenDetailsOfOwnerByIndex(msg.sender, _tokenIndex);
         if (eventId != _eventId) revert InvalidToken();
         ISemaphore(_semaphore).addMember(_groupId, _identityCommitment);
     }
--- packages/plugins/test/unit/paymaster/GateKeeper.t.sol
+++ packages/plugins/test/unit/paymaster/GateKeeper.t.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.12;
 
-import "forge-std/Test.sol";
-import "forge-std/console2.sol";
-import {TestHelper} from "../utils/TestHelper.sol";
-import {PoapSemaphoreGatekeeper} from "../../../src/paymaster/Gatekeeper.sol";
-import {ISemaphore} from "../../../src/paymaster/interfaces/ISemaphore.sol";
+import 'forge-std/Test.sol';
+import 'forge-std/console.sol';
+import {TestHelper} from '../utils/TestHelper.sol';
+import {PoapSemaphoreGatekeeper} from '../../../src/paymaster/Gatekeeper.sol';
+import {ISemaphore} from '../../../src/paymaster/interfaces/ISemaphore.sol';
 
 /* solhint-disable func-name-mixedcase */
 
@@ -16,23 +16,50 @@ contract GateKeeperTest is TestHelper {
     address poap = address(2);
     PoapSemaphoreGatekeeper public gateKeeper;
 
+    ISemaphore.SemaphoreProof validProof =
+        ISemaphore.SemaphoreProof(
+            0,
+            0,
+            0,
+            0,
+            0,
+            [uint(1), uint(2), uint(3), uint(4), uint(5), uint(6), uint(7), uint(8)]
+        );
+
     function setUp() public {
         gateKeeper = new PoapSemaphoreGatekeeper(semaphore, poap, 12);
         bytes memory createGroupCall = abi.encodeWithSelector(
-            bytes4(keccak256("createGroup(address)")),
+            bytes4(keccak256('createGroup(address)')),
             address(gateKeeper)
         );
         _mockAndExpect(semaphore, createGroupCall, abi.encode(100));
         gateKeeper.init();
     }
 
-    function test_validate() public {}
+    function test_should_call_validate_if_verify() public {
+        _mockAndExpect(
+            semaphore,
+            abi.encodeWithSelector(ISemaphore.verifyProof.selector, 100, validProof),
+            abi.encode(true)
+        );
+        _mockAndExpect(
+            semaphore,
+            abi.encodeWithSelector(ISemaphore.validateProof.selector, 100, validProof),
+            abi.encode()
+        );
+        assertTrue(gateKeeper.validate(validProof));
+    }
 
-    function _mockAndExpect(
-        address _target,
-        bytes memory _call,
-        bytes memory _ret
-    ) internal {
+    function test_should_not_validate_if_not_verify() public {
+        _mockAndExpect(
+            semaphore,
+            abi.encodeWithSelector(ISemaphore.verifyProof.selector, 100, validProof),
+            abi.encode(false)
+        );
+        assertFalse(gateKeeper.validate(validProof));
+    }
+
+    function _mockAndExpect(address _target, bytes memory _call, bytes memory _ret) internal {
         vm.mockCall(_target, _call, _ret);
         vm.expectCall(_target, _call);
     }
