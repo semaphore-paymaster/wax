fdb7d33a779b410116a8977d99d5645b043207e7
zkFriendly <zkfriendly@proton.me>
2024-07-05 12:16:25 +0200

    feat: paymaster with no proof invalidation

---
+++ packages/plugins/src/paymaster/SemaphorePaymasterDev.sol
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.4 <0.9.0;
+
+import {IEntryPoint} from 'account-abstraction/interfaces/IEntryPoint.sol';
+import {BasePaymaster} from 'account-abstraction/core/BasePaymaster.sol';
+import {UserOperationLib} from 'account-abstraction/core/UserOperationLib.sol';
+import 'account-abstraction/core/Helpers.sol';
+import {PackedUserOperation} from 'account-abstraction/interfaces/PackedUserOperation.sol';
+import {ISemaphore} from './interfaces/ISemaphore.sol';
+
+/// @title A paymaster that pays for all semeaphore members.
+contract SemaphorePaymaster is BasePaymaster {
+    using UserOperationLib for PackedUserOperation;
+
+    address private immutable _semaphore;
+    uint256 private immutable _groupId;
+
+    /**
+     * Constructor.
+     * @param _entryPoint - The entry point.
+     * @param __semaphore - The semaphore contract address.
+     * @param __groupId   - The semaphore group id.
+     */
+    constructor(IEntryPoint _entryPoint, address __semaphore, uint256 __groupId) BasePaymaster(_entryPoint) {
+        _semaphore = __semaphore;
+        _groupId = __groupId;
+    }
+
+    /**
+     * Validate a user operation.
+     * @param userOp         - The user operation.
+     * @param requiredPreFund - The required pre-fund.
+     */
+    function _validatePaymasterUserOp(
+        PackedUserOperation calldata userOp,
+        bytes32 /*userOpHash*/,
+        uint256 requiredPreFund
+    ) internal view override returns (bytes memory context, uint256 validationData) {
+        (requiredPreFund);
+        (uint48 validUntil, uint48 validAfter, bytes memory signature) = parsePaymasterAndData(userOp.paymasterAndData);
+
+        ISemaphore.SemaphoreProof memory proof = abi.decode(signature, (ISemaphore.SemaphoreProof));
+
+        if (ISemaphore(_semaphore).verifyProof(_groupId, proof)) {
+            return (signature, _packValidationData(false, validUntil, validAfter));
+        }
+        return ('', _packValidationData(true, validUntil, validAfter));
+    }
+
+    /**
+     * Post operation.
+     * @param context               - The context.
+     * @param actualGasCost         - The actual gas cost.
+     * @param actualUserOpFeePerGas - The actual user operation fee per gas.
+     */
+    function _postOp(
+        PostOpMode,
+        bytes calldata context,
+        uint256 actualGasCost,
+        uint256 actualUserOpFeePerGas
+    ) internal override {
+        // must nullifie the semaphore proof. But we don't do that for ease of demonstration.
+    }
+
+    /**
+     * Parse the paymaster and data.
+     * @param paymasterAndData - The paymaster and data.
+     * @return validUntil - The valid until.
+     * @return validAfter  - The valid after.
+     * @return signature   - The signature.
+     */
+    function parsePaymasterAndData(
+        bytes calldata paymasterAndData
+    ) internal pure returns (uint48 validUntil, uint48 validAfter, bytes memory signature) {
+        (, validUntil, validAfter, signature) = abi.decode(paymasterAndData, (address, uint48, uint48, bytes));
+    }
+}
